2ea771db517566491a3be69a1cdf68abb733a186
Christian Fuchs <kontaktierbar@gmail.com>
2026-09-09 22:23:29 +0200

    shorten splash time, add animation

--- js/zjv-splash.js
+++ js/zjv-splash.js
@@ -1,13 +1,14 @@
 // Custom element: <zjv-splash>
 // Shows a brief branded splash screen, then fades out and removes itself.
 
-const HOLD_MS = 1500;
+const HOLD_MS = 1200;
 
 class ZjvSplash extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <img src="/zjv-logo.png" alt="ZJV Logo">
-            <span>Zürcher Judo und Ju-Jitsu Verband</span>`;
+            <span class="zjv-splash-title">Zürcher Judo und Ju-Jitsu Verband</span>
+            <div class="zjv-splash-dots"><span></span><span></span><span></span></div>`;
 
         setTimeout(() => {
             this.classList.add('is-hidden');
